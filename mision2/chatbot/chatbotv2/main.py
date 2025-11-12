f0a99cecd2b3c81bf56fd70390e717fa34a515d3
juan jose <bolihurtado14@gmail.com>
2025-11-12 12:31:20 -0500

    12/11/25

--- mision2/chatbot/chatbotv2/main.py
+++ mision2/chatbot/chatbotv2/main.py
@@ -3,14 +3,14 @@ from chatbot.model import build_and_train_model, predict_answer, load_model
 def main():
     model,vectorizer,unique_answers=load_model()
     if model is None:
-        model,vectorizer,unique_answers=build_and_train_model(training_data)
-        print("\n 😎 chatbot listo. Escribe salir para terminar. \n")
-        while True:
-            user = input("Tu: ").strip()
-            if user.lower() in {"salir","exit","quit"}:
-                print("bot: Hasta Pronto")
-                break
-            response = predict_answer(model, vectorizer, unique_answers,user)
-            print("Bot", response)
+        model,vectorizer,unique_answers=buid_and_train_model(training_data)
+    print("\n 🤖 chatbot listo. Escribe salir para terminar. \n")
+    while True:
+        user = input("Tu: ").strip()
+        if user.lower() in {"salir","exit","quit"}:
+            print("Bot: ¡Hasta pronto!")
+            break
+        response = predict_answer(model, vectorizer, unique_answers,user)
+        print("Bot:", response)
 if __name__=="__main__":
     main()
